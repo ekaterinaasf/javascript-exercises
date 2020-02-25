1026bb8383c622471990eef2c44cd802e6b112c1
Ekaterina <nataveres@gmail.com>
2020-02-25 20:46:27 +0100

    Fibonacci exercise finished

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+  let a = 0;
+  let b = 1;
+  let c = 0;
+  for (let i = 1; i < n; i++) {
+    c = a + b;
+    a = b;
+    b = c;
+  }
+  return c;
+};
 
-}
-
-module.exports = fibonacci
+module.exports = fibonacci;
