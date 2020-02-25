4b816a5c729b7bb5998a13ed54b32fae8364b27e
Ekaterina <nataveres@gmail.com>
2020-02-25 21:07:36 +0100

    Find the oldest finished

--- findTheOldest/findTheOldest.js
+++ findTheOldest/findTheOldest.js
@@ -1,5 +1,35 @@
-let findTheOldest = function() {
+var thisYear = new Date().getFullYear();
 
-}
+let findTheOldest = function(obj) {
+  let arr = []; //to store ages
+  for (let i in obj) {
+    if (obj[i].hasOwnProperty("yearOfDeath")) {
+      arr.push(obj[i].yearOfDeath - obj[i].yearOfBirth);
+    } else {
+      arr.push(thisYear - obj[i].yearOfBirth);
+    }
+  }
+  return obj[arr.indexOf(Math.max(...arr))];
+};
 
-module.exports = findTheOldest
+const people = [
+  {
+    name: "Carly",
+    yearOfBirth: 1942,
+    yearOfDeath: 1970
+  },
+  {
+    name: "Ray",
+    yearOfBirth: 1962,
+    yearOfDeath: 2011
+  },
+  {
+    name: "Jane",
+    yearOfBirth: 1912,
+    yearOfDeath: 1941
+  }
+];
+
+findTheOldest(people).name;
+
+module.exports = findTheOldest;
