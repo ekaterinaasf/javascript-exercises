fba25d94ac86d6c4c51e5761812c1093288d84d0
Ekaterina <nataveres@gmail.com>
2020-02-25 20:28:31 +0100

    Calculator exercise finished

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,32 +1,46 @@
-function add () {
-	
+function add(a, b) {
+  return a + b;
 }
 
-function subtract () {
-	
+function subtract(a, b) {
+  return a - b;
 }
 
-function sum () {
-	
+function sum(arr) {
+  return arr.reduce((a, b) => a + b);
 }
 
-function multiply () {
-	
+function multiply(arr) {
+  return arr.reduce((a, b) => a * b);
 }
 
-function power() {
-	
+function power(x, y) {
+  return Math.pow(x, y);
 }
 
-function factorial() {
-	
+function factorial(num) {
+  if (num === 0) {
+    return 1;
+  } else {
+    return num * factorial(num - 1);
+  }
 }
+
+console.log("2+2=", add(2, 2));
+console.log("10-4=", subtract(10, 4));
+console.log("Sum of 1+3+5+7+9=25? ", sum([1, 3, 5, 7, 9]));
+console.log(
+  "multiply([2,4,6,8,10,12,14])=645120? ",
+  multiply([2, 4, 6, 8, 10, 12, 14])
+);
+console.log("power(4,3)=64? ", power(4, 3));
+console.log("factorial(5)=120? ", factorial(5));
 
 module.exports = {
-	add,
-	subtract,
-	sum,
-	multiply,
-    power,
-	factorial
-}
+  add,
+  subtract,
+  sum,
+  multiply,
+  power,
+  factorial
+};
